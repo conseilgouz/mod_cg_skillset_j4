71599cff24cbfc2b13959f7b903379ec25823ab6
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-17 17:15:25 +0200

    1.2.1
    Use ConseilGouz Library

---
+++ script.php
@@ -0,0 +1,191 @@
+<?php
+/**
+* CG SkillSet - Joomla 4.x/5.x/6.x Module
+* copyright 		: Copyright (C) 2025 ConseilGouz. All rights reserved.
+* license    		: http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+* from              : Joomla 3.x Polished Geek Responsive Post-it module
+*/
+// No direct access to this file
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
+use Joomla\CMS\Version;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
+
+class mod_cg_skillsetInstallerScript
+{
+    private $min_joomla_version      = '4.2.0';
+    private $min_php_version         = '8.0';
+    private $name                    = 'Module CG Skillset';
+    private $exttype                 = 'module';
+    private $extname                 = 'cg_skillset';
+    private $previous_version        = '';
+    private $newlib_version			 = '';
+    private $dir           = null;
+    private $lang;
+    private $installerName = 'mod_cg_skillsetinstaller';
+    public function __construct()
+    {
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
+    }
+
+    public function preflight($type, $parent)
+    {
+        if (! $this->passMinimumJoomlaVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+
+        if (! $this->passMinimumPHPVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+        // To prevent installer from running twice if installing multiple extensions
+        if (! file_exists($this->dir . '/' . $this->installerName . '.xml')) {
+            return true;
+        }
+    }
+
+    public function postflight($type, $parent)
+    {
+        if (($type == 'install') || ($type == 'update')) { // remove obsolete dir/files
+            $this->postinstall_cleanup();
+        }
+
+        switch ($type) {
+            case 'install': $message = Text::_('ISO_POSTFLIGHT_INSTALLED');
+                break;
+            case 'uninstall': $message = Text::_('ISO_POSTFLIGHT_UNINSTALLED');
+                break;
+            case 'update': $message = Text::_('ISO_POSTFLIGHT_UPDATED');
+                break;
+            case 'discover_install': $message = Text::_('ISO_POSTFLIGHT_DISC_INSTALLED');
+                break;
+        }
+        return true;
+    }
+    private function postinstall_cleanup()
+    {
+
+        if (!$this->checkLibrary('conseilgouz')) { // need library installation
+            $ret = $this->installPackage('lib_conseilgouz');
+            if ($ret) {
+                Factory::getApplication()->enqueueMessage('ConseilGouz Library ' . $this->newlib_version . ' installed', 'notice');
+            }
+        }
+        $this->delete([sprintf("%s/modules/mod_%s/src/Field/VersionField.php", JPATH_SITE, $this->extname)]);
+
+        // remove obsolete update sites
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__update_sites')
+            ->where($db->quoteName('location') . ' like "%432473037d.url-de-test.ws/%"');
+        $db->setQuery($query);
+        $db->execute();
+
+    }
+    private function checkLibrary($library)
+    {
+        $file = $this->dir.'/lib_conseilgouz/conseilgouz.xml';
+        if (!is_file($file)) {// library not installed
+            return false;
+        }
+        $xml = simplexml_load_file($file);
+        $this->newlib_version = $xml->version;
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $conditions = array(
+             $db->qn('type') . ' = ' . $db->q('library'),
+             $db->qn('element') . ' = ' . $db->quote($library)
+            );
+        $query = $db->getQuery(true)
+                ->select('manifest_cache')
+                ->from($db->quoteName('#__extensions'))
+                ->where($conditions);
+        $db->setQuery($query);
+        $manif = $db->loadObject();
+        if ($manif) {
+            $manifest = json_decode($manif->manifest_cache);
+            if ($manifest->version >= $this->newlib_version) { // compare versions
+                return true; // library ok
+            }
+        }
+        return false; // need library
+    }
+    private function installPackage($package)
+    {
+        $tmpInstaller = new Joomla\CMS\Installer\Installer();
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $tmpInstaller->setDatabase($db);
+        $installed = $tmpInstaller->install($this->dir . '/' . $package);
+        return $installed;
+    }
+
+    // Check if Joomla version passes minimum requirement
+    private function passMinimumJoomlaVersion()
+    {
+        $j = new Version();
+        $version = $j->getShortVersion();
+        if (version_compare($version, $this->min_joomla_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+                'error'
+            );
+
+            return false;
+        }
+
+        return true;
+    }
+
+    // Check if PHP version passes minimum requirement
+    private function passMinimumPHPVersion()
+    {
+
+        if (version_compare(PHP_VERSION, $this->min_php_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+
+        return true;
+    }
+    private function uninstallInstaller()
+    {
+        if (! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+            return;
+        }
+        $this->delete([
+            JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+            JPATH_PLUGINS . '/system/' . $this->installerName,
+        ]);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+        $db->setQuery($query);
+        $db->execute();
+        Factory::getCache()->clean('_system');
+    }
+    public function delete($files = [])
+    {
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                Folder::delete($file);
+            }
+
+            if (is_file($file)) {
+                File::delete($file);
+            }
+        }
+    }
+
+}
